3f167416419e578423821738a0a10fc33668ce28
Tyler Merritt <tyler@macbook-pro.lan>
2019-08-29 14:42:08 -0500

    Even less JS

--- app/assets/javascripts/visitors.js
+++ app/assets/javascripts/visitors.js
@@ -15,16 +15,6 @@ function defaultPosition(position) {
 }
 
 function createLinkOnPage(latitude, longitude) {
-    buildElement(latitude, longitude);
-    // var link = document.createElement('a');
-    // link.setAttribute('class', 'link_style');
-    // link.innerHTML = "Start";
-    // link.setAttribute('href', "/conversations?latitude=" + latitude + "&longitude=" + longitude);
-    // document.getElementById('link_to_start').appendChild(link);
-    // document.getElementById('lds-roller').remove();
-}
-
-function buildElement(latitude, longitude) {
     setInterval(function () {
         if (document.getElementById("lds-roller")) {
             var link = document.createElement('a');
@@ -36,6 +26,6 @@ function buildElement(latitude, longitude) {
             clearInterval();
         }
     }, 100);
-};
+}
 
 getLocation();
