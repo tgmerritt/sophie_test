b893361127bf2a76bc7cef14457895ec349ba80e
Tyler Merritt <tyler@macbook-pro.lan>
2019-08-29 11:33:26 -0500

    Remove unnecessary code

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,9 +2,6 @@ class ConversationsController < ApplicationController
   before_action :set_conversation, only: [:show, :edit, :update, :destroy]
   skip_before_action :verify_authenticity_token, only: :create
 
-
-  # GET /conversations
-  # GET /conversations.json
   def index
     Conversation.new.authenticate_to_faceme(params)
 
@@ -12,50 +9,12 @@ class ConversationsController < ApplicationController
     @api_key = Rails.application.secrets.fm_api_key
   end
 
-  # GET /conversations/1
-  # GET /conversations/1.json
-  def show
-  end
-
-  # GET /conversations/new
-  def new
-    @conversation = Conversation.new
-  end
-
-  # GET /conversations/1/edit
-  def edit
-  end
-
-  # POST /conversations
-  # POST /conversations.json
   def create
+    # Change the second parameter to another NLP provider in order to query against that provider
+    # You could also implement a custom cascading check against multiple NLP providers.
     orchestration = Orchestration.new(params, "Houndify")
     response = orchestration.orchestrate
     render json: response
-  end
-
-  # PATCH/PUT /conversations/1
-  # PATCH/PUT /conversations/1.json
-  def update
-    respond_to do |format|
-      if @conversation.update(conversation_params)
-        format.html { redirect_to @conversation, notice: 'Conversation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @conversation }
-      else
-        format.html { render :edit }
-        format.json { render json: @conversation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /conversations/1
-  # DELETE /conversations/1.json
-  def destroy
-    @conversation.destroy
-    respond_to do |format|
-      format.html { redirect_to conversations_url, notice: 'Conversation was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
