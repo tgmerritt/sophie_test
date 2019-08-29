c73faa00e1c23e3cb81148d0edc37b639d314cc1
Tyler Merritt <tyler@macbook-pro.lan>
2019-08-29 11:20:14 -0500

    Refactor Houndify methods

--- app/classes/houndify.rb
+++ app/classes/houndify.rb
@@ -27,6 +27,18 @@ class Houndify
     }
   end
 
+  def query_houndify(location, state, query)
+    hound = Houndify.new
+    hound.set_conversation_state(JSON.parse(state)) if state # UneeQ returns conversation state to us stringified, so we have to unravel that and make it a JSON object again
+    if !location["latitude"].blank? && !location["longitude"].blank?
+        hound.set_location(location["latitude"].to_f,  location["longitude"].to_f)
+    end
+    # puts "Hound object before calling query"
+    # puts hound.inspect
+    response = hound.query(query)
+    setup_houndify_json(response)
+  end
+
   def set_hound_request_info(key, value)
     """
     There are various fields in the hound_request_info object that can
@@ -129,5 +141,94 @@ class Houndify
     rescue
       return { "Error": response }
     end
+  end
+
+  def setup_houndify_json(response)
+    handle_houndify_daily_limit(response)
+  end
+
+  def handle_houndify_daily_limit(response)
+    @response = response
+    if @response[:Error]
+        create_json_to_send("Sorry, I cannot answer that right now", nil)
+    else
+        text = @response["AllResults"][0]["WrittenResponseLong"]
+        create_json_to_send(text, houndify_combined_html(houndify_html, houndify_html_assets))
+    end
+  end
+
+  def houndify_combined_html(html, html_assets)
+    if html.nil?
+        combined_html = nil
+    else
+        css = html_assets["CSS"].gsub(/\"/, '\'')
+        js = html_assets["JS"].gsub(/\"/, '\'')
+        combined_html = css + js + html
+        combined_html.gsub!(/[\r\n]+/, ' ')
+    end
+  end
+
+  def houndify_html_assets
+      if @response["AllResults"][0]["HTMLData"] && @response["AllResults"][0]["HTMLData"]["HTMLHead"]
+          @response["AllResults"][0]["HTMLData"]["HTMLHead"]
+      else
+          nil
+      end
+  end
+
+  def houndify_html
+      if @response["AllResults"][0]["HTMLData"] && @response["AllResults"][0]["HTMLData"]["SmallScreenHTML"]
+          @response["AllResults"][0]["HTMLData"]["SmallScreenHTML"]
+      else
+          nil
+      end
+  end
+
+  def houndify_conversation_state
+      if @response["AllResults"] && @response["AllResults"][0]["ConversationState"]
+          generate_json_string(@response["AllResults"][0]["ConversationState"])
+      else
+          ""
+      end
+  end
+
+  def generate_json_string(data)
+      JSON.generate(data)
+  end
+
+  def create_json_to_send(text, html)
+      answer_body = {
+          "answer": text,
+          "instructions": {
+              # "expressionEvent": [
+              #     {
+              #         "expression": "headPitch", # string, a supported expression in lowerCamelCase
+              #         "value": 0.5, # number, intensity. Range varies depending on the expression
+              #         "start": 2 # number, in seconds from start of the utterance
+              #         "duration": 5 # number, duration in seconds this expression 
+              #     }
+              # ],
+              "emotionalTone": [
+                  {
+                      "tone": "happiness", # desired emotion in lowerCamelCase
+                      "value": 0.5, # number, intensity of the emotion to express between 0.0 and 1.0 
+                      "start": 2, # number, in seconds from the beginning of the utterance to display the emotion
+                      "duration": 4, # number, duration in seconds this emotion should apply
+                      "additive": true, # boolean, whether the emotion should be added to existing emotions (true), or replace existing ones (false)
+                      "default": true # boolean, whether this is the default emotion 
+                  }
+              ],
+              "displayHtml": {
+                  "html": html
+              }
+          }
+      }
+
+      body = {
+          "answer": generate_json_string(answer_body),         
+          "matchedContext": "",
+          "conversationPayload": houndify_conversation_state,
+      }
+      return body
   end
 end
--- app/classes/orchestration.rb
+++ app/classes/orchestration.rb
@@ -9,121 +9,16 @@ class Orchestration
         @response = nil
     end
 
+    def orchestrate
+        determine_partner
+    end
+
     def determine_partner
         case @partner
         when "Houndify"
-            query_houndify
+            Houndify.new.query_houndify(@location, @conversation_state, @query)
         else 
             return nil
         end
-    end
-
-    def query_houndify
-        hound = Houndify.new
-        hound.set_conversation_state(JSON.parse(@conversation_state)) if @conversation_state # UneeQ returns conversation state to us stringified, so we have to unravel that and make it a JSON object again
-        if !@location["latitude"].blank? && !@location["longitude"].blank?
-            hound.set_location(@location["latitude"].to_f,  @location["longitude"].to_f)
-        end
-        # puts "Hound object before calling query"
-        # puts hound.inspect
-        @response = hound.query(@query)
-    end
-
-    def orchestrate
-        determine_partner
-        map_response_to_uneeq_json
-    end
-
-    def map_response_to_uneeq_json
-        case @partner
-        when "Houndify"
-            handle_houndify_daily_limit
-        else
-            return nil
-        end
-    end
-
-    def handle_houndify_daily_limit
-        if @response[:Error]
-            create_json_to_send("Sorry, I cannot answer that right now", nil)
-        else
-            text = @response["AllResults"][0]["WrittenResponseLong"]
-            create_json_to_send(text, houndify_combined_html(houndify_html, houndify_html_assets))
-        end
-    end
-
-    def houndify_combined_html(html, html_assets)
-        if html.nil?
-            combined_html = nil
-        else
-            css = html_assets["CSS"].gsub(/\"/, '\'')
-            js = html_assets["JS"].gsub(/\"/, '\'')
-            combined_html = css + js + html
-            combined_html.gsub!(/[\r\n]+/, ' ')
-        end
-    end
-
-    def houndify_html_assets
-        if @response["AllResults"][0]["HTMLData"] && @response["AllResults"][0]["HTMLData"]["HTMLHead"]
-            @response["AllResults"][0]["HTMLData"]["HTMLHead"]
-        else
-            nil
-        end
-    end
-
-    def houndify_html
-        if @response["AllResults"][0]["HTMLData"] && @response["AllResults"][0]["HTMLData"]["SmallScreenHTML"]
-            @response["AllResults"][0]["HTMLData"]["SmallScreenHTML"]
-        else
-            nil
-        end
-    end
-
-    def houndify_conversation_state
-        if @response["AllResults"] && @response["AllResults"][0]["ConversationState"]
-            generate_json_string(@response["AllResults"][0]["ConversationState"])
-        else
-            ""
-        end
-    end
-
-    def generate_json_string(data)
-        JSON.generate(data)
-    end
-
-    def create_json_to_send(text, html)
-        answer_body = {
-            "answer": text,
-            "instructions": {
-                # "expressionEvent": [
-                #     {
-                #         "expression": "headPitch", # string, a supported expression in lowerCamelCase
-                #         "value": 0.5, # number, intensity. Range varies depending on the expression
-                #         "start": 2 # number, in seconds from start of the utterance
-                #         "duration": 5 # number, duration in seconds this expression 
-                #     }
-                # ],
-                "emotionalTone": [
-                    {
-                        "tone": "happiness", # desired emotion in lowerCamelCase
-                        "value": 0.5, # number, intensity of the emotion to express between 0.0 and 1.0 
-                        "start": 2, # number, in seconds from the beginning of the utterance to display the emotion
-                        "duration": 4, # number, duration in seconds this emotion should apply
-                        "additive": true, # boolean, whether the emotion should be added to existing emotions (true), or replace existing ones (false)
-                        "default": true # boolean, whether this is the default emotion 
-                    }
-                ],
-                "displayHtml": {
-                    "html": html
-                }
-            }
-        }
-
-        body = {
-            "answer": generate_json_string(answer_body),         
-            "matchedContext": "",
-            "conversationPayload": houndify_conversation_state,
-        }
-        return body
     end
 end
