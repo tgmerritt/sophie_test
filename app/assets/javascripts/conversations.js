090dc4d88e077fa2d8420aad4d283d2c47ff93d4
Tyler Merritt <tgmerritt@gmail.com>
2020-02-18 18:02:39 -0600

    conversationId for EB

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -9,7 +9,7 @@ window.onload = function () {
 
     const uneeq = new Uneeq({
         url: 'https://api.us.uneeq.io',
-        conversationId: '9c7dafd7-2d90-49aa-b66a-2dfc394865e1',
+        conversationId: '1507ece5-dafd-49fd-8b8b-699f40836f43',
         // conversationId: '618ac47d-7e60-413e-9381-0e4671e6f572', // This conversation ID is for local debugging only
         avatarVideoContainerElement: document.getElementById('avatar-container'),
         localVideoContainerElement: document.getElementById('local-container'),
